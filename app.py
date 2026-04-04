4eb844652a325846d5e81f7ae124f01fd7585638
HariniV2907 <hariniv2907@gmail.com>
2026-04-04 16:23:52 +0000

    Added app.py

--- app.py
+++ app.py
@@ -1,1 +1,18 @@
- 
+
+# load the model
+model = joblib.load("model.pkl")
+
+# title of the application
+st.title("Tourism Prediction App")
+st.write("Enter details below:")
+
+# taking user inputs from UI
+age = st.number_input("Enter Age: ")
+income = st.number_input("Enter Income: ")
+duration = st.number_input("Enter Trip Duration: ")
+
+# prediction button
+if st.button("Predict"):
+    input_data = pd.DataFrame([[age, income, duration]])
+    prediction = model.predict(input_data)
+    st.success(f"Prediction: {prediction[0]}")
